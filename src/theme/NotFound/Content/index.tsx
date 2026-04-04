4774f7b6323cc0bedda0ae0cfc5426453d5b3419
Taran Mittal <taranmittal09@gmail.com>
2026-04-04 23:51:11 +1100

    does this work?

---
+++ src/theme/NotFound/Content/index.tsx
@@ -0,0 +1,37 @@
+import React, {type ReactNode} from 'react';
+import clsx from 'clsx';
+import Translate from '@docusaurus/Translate';
+import type {Props} from '@theme/NotFound/Content';
+import Heading from '@theme/Heading';
+
+export default function NotFoundContent({className}: Props): ReactNode {
+  return (
+    <main className={clsx('container margin-vert--xl', className)}>
+      <div className="row">
+        <div className="col col--6 col--offset-3">
+          <Heading as="h1" className="hero__title">
+            <Translate
+              id="theme.NotFound.title"
+              description="The title of the 404 page">
+              Page Not Found
+            </Translate>
+          </Heading>
+          <p>
+            <Translate
+              id="theme.NotFound.p1"
+              description="The first paragraph of the 404 page">
+              404: We could not find what you were looking for.
+            </Translate>
+          </p>
+          <p>
+            <Translate
+              id="theme.NotFound.p2"
+              description="The 2nd paragraph of the 404 page">
+              Were you trying to look for something else? <a href="/">Go home</a>.
+            </Translate>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}

ddb484bd1f5b48f5e5221f1ce4f2aa76a2d7baaf
Taran Mittal <taranmittal09@gmail.com>
2026-04-11 02:07:18 +1000

    does this work?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -88,7 +88,7 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          <img src="/img/logo-full.png" alt={siteConfig.title} />
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>

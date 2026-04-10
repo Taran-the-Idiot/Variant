05ead57def6806655ff77f2cde6c8938a21d04e9
Taran Mittal <taranmittal09@gmail.com>
2026-04-11 01:16:20 +1000

    banana

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -94,7 +94,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/How-To-Start/intro">
+            to="/docs/getting-started/intro">
             Get Started!
           </Link>
         </div>

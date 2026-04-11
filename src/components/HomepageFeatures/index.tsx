56b579d96dbe021ee29c7a582d58263eb51e3941
Taran Mittal <taranmittal09@gmail.com>
2026-04-11 21:53:07 +1000

    blehtornadus

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -20,20 +20,20 @@ const FeatureList: FeatureItem[] = [
     ),
   },
   {
-    title: 'Make it!',
+    title: 'Design it!',
     Svg: '/img/repo.png',
     description: (
       <>
-        Follow our guides to make your project, and get help from the community if you need it!
+        Follow our guides to design your project, and get help from the community if you need it!
       </>
     ),
   },
   {
-    title: 'Submit!',
+    title: 'Make it Real!',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Submit your project <a href="/docs/How-To-Start/submit" target="_blank" rel="noopener noreferrer">here</a> and profit!
+        Get funding to build your project irl, then share it with the world when it's done!
       </>
     ),
   },
